0071b424ec6fd050e5201d231b9b4b28d14c9d45
pitasuke <pitrial1151@gmail.com>
2022-07-06 17:26:00 +0900

    n3

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 //現在時刻取得
 var dt = new Date();
-
 //年を抽出
 var year = dt.getFullYear();
 //月を抽出
 var month = dt.getMonth()+1;
 //曜日を抽出
-var date = dt.getDate()+1;
-//日付を抽出
 var day = dt.getDay();
+//日にち
+var date = dt.getDate();
+var firstDate = new Date(year,month-1,1);
+var firstDay= firstDate.getDay();
+var dayarray=[];
+var lastdate = new Date( year, month, 0);
+var lastDate = lastdate.getDate();
+console.log(firstDay);
+console.log(lastDate)
 
-var firstDate = 6 - (day % 7);
-console.log(firstDate);
+for(let i = 1; i<=42 ; i++){
+if(i<=firstDay){
+    dayarray.push(" ");
+}
+else if(i<=firstDay+lastDate){
+    dayarray.push(i-firstDay);
+}
+else{
+    dayarray.push(" ");
+};
+};
+console.log(dayarray)
+
